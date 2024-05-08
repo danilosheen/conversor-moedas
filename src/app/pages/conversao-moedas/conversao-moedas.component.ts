56bb491e3868ddfcff16b3de1499be0722524b94
danilosheen <c.danilo.f.silva@gmail.com>
2024-05-08 12:58:03 -0300

    feat: página de conversão de moedas

--- src/app/pages/conversao-moedas/conversao-moedas.component.ts
+++ src/app/pages/conversao-moedas/conversao-moedas.component.ts
@@ -3,14 +3,13 @@ import { ExchangeApiService } from '../../services/exchange-api.service';
 import { IConversor } from '../../interfaces/iresponse-api-convert';
 import { Moedas } from '../../interfaces/imoedas';
 import { FormsModule } from '@angular/forms';
-import { NgFor } from '@angular/common';
-import { TablePaginationExample } from '../../components/table-pagination/table-pagination.component';
+import { NgFor, NgIf } from '@angular/common';
 import { ListaSiglasService } from '../../services/lista-siglas.service';
 
 @Component({
   selector: 'app-conversao-moedas',
   standalone: true,
-  imports: [FormsModule, NgFor],
+  imports: [FormsModule, NgFor, NgIf],
   templateUrl: './conversao-moedas.component.html',
   styleUrl: './conversao-moedas.component.css',
 })
@@ -21,6 +20,7 @@ export class ConversaoMoedasComponent implements OnInit {
   valor: string = '';
   siglas: string[] = [];
   moedas: string[] = [];
+  response!: IConversor;
 
   constructor(
     private conversorApi: ExchangeApiService,
@@ -41,8 +41,12 @@ export class ConversaoMoedasComponent implements OnInit {
       )
       .subscribe((dados: IConversor) => {
         this.valorConvertido = parseFloat(dados.conversion_result.toFixed(2));
-        console.log(this.valorConvertido);
+        this.response = dados;
         return this.valorConvertido;
       });
+  }
+
+  getResponseCompleta() {
+    return this.response;
   }
 }
